3938c3ed4c266a2aeb5ad0c1ceb7060a9aef9cff
星 優太 <youtaixing753@gmail.com>
2024-05-13 11:54:57 +0900

    初級Java2.5のコミット

---
+++ 初級Java2.5/src/Start.java
@@ -0,0 +1,17 @@
+
+/**
+ * 
+ */
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+     
+       MyFrame frame3 = new RecFrame();
+       frame3.setLocation(50,300);
+       MyFrame frame4 = new RecFrame();
+       frame4.setLocation(100,300);
+	
+	}
+
+}
