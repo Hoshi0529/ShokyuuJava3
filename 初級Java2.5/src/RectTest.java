a4d86095acc1ccafaf61d90befcd278adde67fc6
星 優太 <youtaixing753@gmail.com>
2024-05-14 12:10:56 +0900

    色色

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -2,9 +2,20 @@
 public class RectTest extends MyFrame {
 	
 	public void  run() {
+		//黄色
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		//紫
+		setColor(255,0,255);
 		fillRect(130,40,50,100);
+		//水色
+		setColor(0,255,255);
 		fillRect(30,200,150,10);
-		fillRect(90,160,30,30);
+		//黒
+		setColor(0,0,0);
+		fillRect(70,160,30,30);
+		//灰色
+		setColor(155,155,155);
+		fillRect(110,160,30,30);
 	}
 }
