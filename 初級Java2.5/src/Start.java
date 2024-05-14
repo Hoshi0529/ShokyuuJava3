5899a5835da9e5a057e0e04075b81dfcfe5f8552
星 優太 <youtaixing753@gmail.com>
2024-05-14 10:46:28 +0900

    今日のコミット１

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -7,11 +7,8 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
      
-       MyFrame frame3 = new RecFrame();
-       frame3.setLocation(50,300);
-       MyFrame frame4 = new RecFrame();
-       frame4.setLocation(100,300);
-	
+    System.out.println("プログラムを実行します");
+    MyFrame frame1 = new RectTest();
 	}
 
 }
