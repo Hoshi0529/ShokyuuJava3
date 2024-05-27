3aaa83beb938de9b05fa7946221f0e0f23301e81
星 優太 <youtaixing753@gmail.com>
2024-05-27 12:20:57 +0900

    なんかリセットしてしまう

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -2,25 +2,52 @@
 public class Animation extends MyFrame{
 	 public void run() {
 		 
-		  int y = 100;
+		  int y = 50;
 		  int x =30;
-		 
-		  for(int i=0;i<3;i++){
+		  int nextY = 0;
+		  while(true){
 			 while(x<=170) {
 				 clear();
-		 
+				 y = 50;
 				 setColor(0,128,0);
 				 fillRect(x,y,10,100);
 				 x+=5;
 				 sleep(0.1); 
+				
+			 }
+			
+			 nextY=y+50;
+			 while(y<=nextY) {
+			
+				 clear();
+				 
+				 setColor(0,128,0);
+				 fillRect(x,y,10,100);
+				 y+=5;
+				 sleep(0.1);
+			  
 			 }
 			 
+			 
 			 while(x>=30) {
+			
 				 clear();
 				 setColor(0,128,0);
 				 fillRect(x,y,10,100);
 				 x-=5;
 				 sleep(0.1);
+				
+			 }
+			 nextY=y+50;
+			 while(y<=nextY) {
+				 clear();
+				 
+				 setColor(0,128,0);
+				 fillRect(x,y,10,100);
+				 y+=5;
+				 sleep(0.1);
+				 
+				   
 			 }
 			 
 		  }
