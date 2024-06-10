a041c124ce7bdcc4f1856a87004ce9361f8a1f63
星 優太 <youtaixing753@gmail.com>
2024-06-10 11:44:13 +0900

    練習問題8-1

--- 初級Java2.5/src/DisplayCarsTrains.java
+++ 初級Java2.5/src/DisplayCarsTrains.java
@@ -2,10 +2,13 @@
 public class DisplayCarsTrains  extends MyFrame{
 	
 	public void run() {
-		Vehicle[] vehicles=new Vehicle[3];
+		Vehicle[] vehicles=new Vehicle[6];
 		vehicles[0]=new Car(10,50,3,0);
-		vehicles[1]=new Car(10,100,5,0);
-		vehicles[2]=new Train(10,150,7,0);
+		vehicles[1]=new Car(400,100,-7,-0);
+		vehicles[2]=new Train(10,170,7,0);
+		vehicles[3]=new Train(400,220,-7,-0);
+		vehicles[4]=new Truck(10,270,4,0);
+		vehicles[5]=new Truck(10,320,7,0);        
 		for(int i=0; i<30; i++) {
 			clear();
 		
---
+++ 初級Java2.5/src/Truck.java
@@ -0,0 +1,18 @@
+
+public class Truck extends Vehicle {
+	
+	public Truck(int x,int y,int vx,int vy) {
+		super(x,y,vx,vy);
+	}
+       public void draw(MyFrame frame) {
+		
+        frame.fillRect(x+80, y, 20, 35);
+	    frame.fillRect(x, y+20, 100, 20);
+	    frame.fillOval(x+5, y+50,10, 10);
+		frame.fillOval(x+15, y+50, 10, 10);
+		frame.fillOval(x+85, y+50, 10, 10);
+		
+
+		}
+	
+}
